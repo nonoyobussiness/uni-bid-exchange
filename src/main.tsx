325bfb6e4c1d238956e48c3a42ae088bb0ed3b88
nonoyobussiness <sreevathsal2006@gmail.com>
2026-05-04 20:50:32 +0530

    remove conflicting vercel config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
 
-import App from "./App";
+import "./styles.css";
+import { getRouter } from "./router";
+
+const router = getRouter();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
